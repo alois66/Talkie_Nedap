c6c6e537605fb601bc7360680df6e322928bcb4a
Aloïs <alois.myard@gmail.com>
2015-09-16 18:49:21 +0200

    Correction adresses

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Talkie_Nedap
-Script alarme kalkie
+Script alarme kalkie Nedap
+
+Executez depuis dossier de l'utilisateur principal :
+sudo wget -O - https://dl.dropboxusercontent.com/u/27241306/install.sh | bash
 
 2015-09-16
-Mise en ligne initial du script écrit par Sarakha63
+Correction adresses.
+
+2015-09-16
+Mise en ligne initial du script écrit par Sarakha63.
 
--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 wget https://github.com/alois66/Talkie_Nedap/archive/master.zip
 unzip master.zip
-mkdir Talkie
-cp -r -f master/* Talkie/*
-rm -r -f master
+mkdir Talkie_Nedap
+cp -r -f Talkie_Nedap-master/* Talkie_Nedap/*
+rm -r -f Talkie_Nedap-master
+rm master.zip
 sudo pip install flask
 sudo pip install RPi.GPIO
 sudo apt-get install mplayer
@@ -10,7 +11,7 @@ sudo apt-get install libav-tools
 sudo dpkg -i pico_build/libttspico-data_1.0+git20110131-2_all.deb
 sudo dpkg -i pico_build/libttspico0_1.0+git20110131-2_armhf.deb
 sudo dpkg -i pico_build/libttspico-utils_1.0+git20110131-2_armhf.deb
-sudo cp master/Talkie_init /etc/init.d/
+sudo cp Talkie_Nedap/Talkie_init /etc/init.d/
 sudo chown root:root /etc/init.d/Talkie_init
 sudo chmod 0755 /etc/init.d/Talkie_init
 sudo /etc/init.d/Talkie_init enable
